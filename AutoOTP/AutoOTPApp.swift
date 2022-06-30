3f97ea6f09ad5d0bffa5fd7173ea5e1aa54330d3
alexokhtov <hasanohtov@yandex>
2022-06-30 14:16:11 +0300

    Initial Commit

---
+++ AutoOTP/AutoOTPApp.swift
@@ -0,0 +1,17 @@
+//
+//  AutoOTPApp.swift
+//  AutoOTP
+//
+//  Created by Alex Okhtov on 30.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct AutoOTPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
